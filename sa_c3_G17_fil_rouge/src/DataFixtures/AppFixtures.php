8cb90a34bb68087f3a44bc0b02b478971509c087
MamadouNDAO <itech.ndao@gmail.com>
2020-07-24 10:13:05 +0000

    MAJ SPRINT 1

--- sa_c3_G17_fil_rouge/src/DataFixtures/AppFixtures.php
+++ sa_c3_G17_fil_rouge/src/DataFixtures/AppFixtures.php
@@ -21,9 +21,10 @@ class AppFixtures extends Fixture
     {
         $this->encoder = $encoder;
     }
+
     public function load(ObjectManager $manager)
     {
-        $faker = Factory::create('fr_FR');
+       $faker = Factory::create('fr_FR');
         $profils = [
             "ADMIN",
             "APPRENANT",
@@ -32,20 +33,20 @@ class AppFixtures extends Fixture
         ];
         $size = \count($profils);
         $roles = [];
-        for ($i=0; $i < $size; $i++) {
+        for ($i=0; $i < $size; $i++) { 
             $profil = new Profil();
             $profil->setLibelle($profils[$i]);
             $manager->persist($profil);
             $roles[] = $profil;
         }
-        for ($i=0 ; $i < 10 ; $i++ ) {
+        for ($i=0 ; $i < 10 ; $i++ ) { 
             # code...
             $user = new User();
             $hash = $this->encoder->encodePassword($user, "admin");
             $profil = $roles[rand(0,$size-1)];
             $user->setUsername($faker->firstName)
-                ->setPassword($hash)
-                ->setProfil($profil);
+               ->setPassword($hash)
+               ->setProfil($profil);
             $profil->addUser($user);
             $manager->persist($user);
         }
--- sa_c3_G17_fil_rouge/src/Entity/User.php
+++ sa_c3_G17_fil_rouge/src/Entity/User.php
@@ -2,15 +2,106 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
-use App\Repository\UserRepository;
+use ApiPlatform\Core\Annotation\ApiSubresource;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\UserInterface;
+use App\Repository\UserRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @ApiResource(attributes={"pagination_items_per_page"=5}))
+ * @ApiResource(
+ *     normalizationContext={"groups"={"user:read"}},
+ *     attributes={"pagination_items_per_page"=5},
+ *     collectionOperations={
+ *          "add_user"={
+ *              "method"="POST",
+ *              "path"="/admin/users",
+ *              "security"="is_granted('ROLE_ADMIN') or is_granted('ROLE_CM')",
+ *              "security_message"="Vous n'avez pas access à cette Ressource"
+ *          },
+ *          "get_users"={
+ *              "method"="GET",
+ *              "path"="/admin/users",
+ *              "security"="is_granted('ROLE_ADMIN')",
+ *              "security_message"="Vous n'avez pas access à cette Ressource"
+ *          },
+ *          "get_students"={
+ *              "method"="GET",
+ *              "path"="/apprenants",
+ *              "security"="is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR)",
+ *              "security_message"="Vous n'avez pas access à cette Ressource",
+ *
+ *          },
+ *          "add_user"={
+ *              "method"="POST",
+ *              "path"="/admin/users",
+ *              "security"="is_granted('ROLE_ADMIN') or is_granted('ROLE_CM')",
+ *              "security_message"="Vous n'avez pas access à cette Ressource"
+ *          },
+ *          "add_student"={
+ *              "method"="POST",
+ *              "path"="/apprenants",
+ *              "security"="is_granted('ROLE_ADMIN')",
+ *              "security_message"="Vous n'avez pas access à cette Ressource"
+ *          },
+ *    },
+ *     itemOperations={
+ *          "get_user"={
+ *              "method"="GET",
+ *              "path"="/admin/users/{id}",
+ *              "requirements"={"id"="\d+"},
+ *              "security"="is_granted('ROLE_ADMIN')",
+ *              "security_message"="Vous n'avez pas access à cette Ressource"
+ *          },
+ *          "get_student"={
+ *              "method"="GET",
+ *              "path"="/apprenants/{id}",
+ *              "requirements"={"id"="\d+"},
+ *              "security"="(is_granted('ROLE_ADMIN'))",
+ *              "security_message"="Vous n'avez pas access à cette Ressource"
+ *          },
+ *          "get_formateur"={
+ *              "method"="PUT",
+ *              "path"="/formateurs/{id}",
+ *              "requirements"={"id"="\d+"},
+ *              "security"="(is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR'))",
+ *              "security_message"="Vous n'avez pas access à cette Ressource"
+ *          },
+ *          "delete_user"={
+ *              "method"="DELETE",
+ *              "path"="/admin/users/{id}",
+ *              "requirements"={"id"="\d+"},
+ *              "security"="is_granted('ROLE_ADMIN')",
+ *              "security_message"="Vous n'avez pas access à cette Ressource"
+ *          },
+ *          "set_user"={
+ *              "method"="PUT",
+ *              "path"="/admin/users/{id}",
+ *              "requirements"={"id"="\d+"},
+ *              "security"="is_granted('ROLE_ADMIN')",
+ *              "security_message"="Vous n'avez pas access à cette Ressource"
+ *          },
+ *          "set_student"={
+ *              "method"="PUT",
+ *              "path"="/apprenants/{id}",
+ *              "requirements"={"id"="\d+"},
+ *              "security"="(is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR')) or is_granted('ROLE_CM') or is_granted('ROLE_APPRENANT'))",
+ *              "security_message"="Vous n'avez pas access à cette Ressource"
+ *          },
+ *          "set_formateur"={
+ *              "method"="PUT",
+ *              "path"="/formateurs/{id}",
+ *              "requirements"={"id"="\d+"},
+ *              "security"="(is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR'))",
+ *              "security_message"="Vous n'avez pas access à cette Ressource"
+ *          },
+ *     }
+ *  )
  */
 class User implements UserInterface
 {
@@ -18,35 +109,44 @@ class User implements UserInterface
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"user:read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
-     * @Assert\NotBlank
+     * @Assert\NotBlank(message="Le username est obligatoire")
+     *@Groups({"user:read"})
      */
     private $username;
 
-
+    /**
+     *
+     * @Groups({"user:read"})
+    */
     private $roles = [];
 
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
-     * @Assert\NotBlank
+     * @Assert\NotBlank(message="Le password est obligatoire")
+     * @Groups({"user:read"})
      */
     private $password;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $avatar;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=Profil::class, inversedBy="users")
+     * @ORM\ManyToOne(targetEntity=Profil::class, inversedBy="user")
      * @ORM\JoinColumn(nullable=false)
+     * @ApiSubresource
+     * @Groups({"user:read"})
      */
     private $profil;
+
+    /**
+     * @ORM\Column(type="blob", nullable=true)
+     *
+     */
+    private $avatar;
 
     public function getId(): ?int
     {
@@ -60,7 +160,7 @@ class User implements UserInterface
      */
     public function getUsername(): string
     {
-        return (string) $this->username;
+        return (string)$this->username;
     }
 
     public function setUsername(string $username): self
@@ -77,7 +177,7 @@ class User implements UserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_'.$this->profil->getLibelle();
+        $roles[] = 'ROLE_' . $this->profil->getLibelle();
 
         return array_unique($roles);
     }
@@ -94,12 +194,12 @@ class User implements UserInterface
      */
     public function getPassword(): string
     {
-        return (string) $this->password;
+        return (string)$this->password;
     }
 
     public function setPassword(string $password): self
     {
-        $this->password = $password;
+        $this->password = $password; //$encoder->encodePassword($this,$password);
 
         return $this;
     }
@@ -121,18 +221,6 @@ class User implements UserInterface
         // $this->plainPassword = null;
     }
 
-    public function getAvatar(): ?string
-    {
-        return $this->avatar;
-    }
-
-    public function setAvatar(?string $avatar): self
-    {
-        $this->avatar = $avatar;
-
-        return $this;
-    }
-
     public function getProfil(): ?Profil
     {
         return $this->profil;
@@ -141,6 +229,18 @@ class User implements UserInterface
     public function setProfil(?Profil $profil): self
     {
         $this->profil = $profil;
+
+        return $this;
+    }
+
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+
+    public function setAvatar($avatar): self
+    {
+        $this->avatar = $avatar;
 
         return $this;
     }
