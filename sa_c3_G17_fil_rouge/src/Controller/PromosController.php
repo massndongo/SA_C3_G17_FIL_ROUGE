7854336ddd15f55dacb050e303643096e5021c13
Papa-Souhaibou <souhaibou.toure122@gmail.com>
2020-08-11 14:48:20 +0000

    Gestion itemOperation de l'entite Promo.

--- sa_c3_G17_fil_rouge/src/Controller/PromosController.php
+++ sa_c3_G17_fil_rouge/src/Controller/PromosController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Promos;
 use App\Repository\ApprenantRepository;
 use App\Repository\FormateurRepository;
+use App\Repository\GroupeCompetenceRepository;
 use App\Repository\PromosRepository;
 use App\Repository\ReferentielRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -15,6 +16,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use function Symfony\Component\String\s;
 
 class PromosController extends AbstractController
 {
@@ -30,7 +32,33 @@ class PromosController extends AbstractController
         $this->manager = $manager;
         $this->validator = $validator;
     }
-    
+
+
+    /**
+     * @Route(
+     *     path="/mail",
+     *     name="mail"
+     * )
+     */
+    public function email(\Swift_Mailer $mailer,GroupeCompetenceRepository $repository)
+    {
+
+        $sender = 'terangawebdevelopment@gmail.com';
+        $receiver = "petitpapatoure98@gmail.com";
+        $message = (new \Swift_Message("Ajout apprenant au promo"))
+            ->setFrom($sender)
+            ->setTo($receiver)
+            ->setBody(
+                $this->renderView(
+                    "emails/congratulation.html.twig",["nothing"]
+                ),
+                "text/html"
+            );
+        $mailer->send($message);
+        $grpeCompetence = $repository->findOneBy(["id" => 1]);
+        return $this->json($grpeCompetence,Response::HTTP_OK);
+    }
+
     /**
      * @Route(
      *     path="/api/admin/promos",
@@ -44,7 +72,25 @@ class PromosController extends AbstractController
         $promos = $this->promosRepository->findBy([
             "isDeleted" => false
         ]);
-        return $this->json($promos,Response::HTTP_OK);
+        $promoTab = [];
+        $promoTab["referentiels"] = [];
+        $promoTab["formateurs"] = [];
+        $promoTab["groupes"] = [];
+        foreach ($promos as $promo)
+        {
+            $promoTab["referentiels"][] = $promo->getReferentiel();
+            $formateurs = $promo->getFormateur();
+            foreach ($formateurs as $formateur)
+            {
+                $promoTab["formateurs"][] = $formateur;
+            }
+            $groupes = $promo->getGroupes();
+            foreach ($groupes as $groupe)
+            {
+                $promoTab["groupes"][] = $groupe;
+            }
+        }
+        return $this->json($promoTab,Response::HTTP_OK);
     }
 
     /**
@@ -60,19 +106,24 @@ class PromosController extends AbstractController
         $promos = $this->promosRepository->findBy([
             "isDeleted" => false
         ]);
-        $principaux = [];
+        $promoTab = [];
+        $promoTab["referentiels"] = [];
+        $promoTab["apprenants"] = [];
+        $promoTab["formateurs"] = [];
         foreach ($promos as $promo)
         {
             $groupes = $promo->getGroupes();
+            $promoTab["referentiels"][] = $promo->getReferentiel();
+            $promoTab["formateurs"][] = $promo->getReferentiel();
             foreach ($groupes as $groupe)
             {
                 if (!$groupe->getIsDeleted() && $groupe->getType() == "principal")
                 {
-                    $principaux[] = $promo;
+                    $promoTab["apprenants"][] = $groupe->getApprenant();
                 }
             }
         }
-        return $this->json($principaux,Response::HTTP_OK);
+        return $this->json($promoTab,Response::HTTP_OK);
     }
 
     /**
@@ -88,10 +139,13 @@ class PromosController extends AbstractController
         $promos = $this->promosRepository->findBy([
             "isDeleted" => false
         ]);
-        $waiting = [];
+        $promoTab = [];
+        $promoTab["referentiels"] = [];
+        $promoTab["apprenants"] = [];
         foreach ($promos as $promo)
         {
             $groupes = $promo->getGroupes();
+            $promoTab["referentiels"][] = $promo->getReferentiel();
             foreach ($groupes as $groupe)
             {
                 $students = $groupe->getApprenant();
@@ -99,14 +153,197 @@ class PromosController extends AbstractController
                 {
                     if (!$student->getIsConnected())
                     {
-                        $waiting[] = $promo;
+                        $promoTab["apprenants"][] = $student;
                     }
                 }
             }
         }
-        return $this->json($waiting,Response::HTTP_OK);
+        return $this->json($promoTab,Response::HTTP_OK);
     }
 
+    /**
+     * @Route(
+     *     path="/api/admin/promos/{id}",
+     *     methods={"GET"},
+     *     name="getPromo"
+     * )
+     */
+    public function getPromo($id,PromosRepository $promosRepository)
+    {
+        $this->canViewPromo();
+        $promo = $promosRepository->findOneBy(["id" => $id]);
+        if ($promo && !$promo->getIsDeleted())
+        {
+            return $this->json($promo,Response::HTTP_OK);
+        }
+        return $this->json(["message" => "Ressource inexistante."],Response::HTTP_NOT_FOUND);
+    }
+
+    /**
+     * @Route(
+     *     path="/api/admin/promos/{id}/principal",
+     *     methods={"GET"},
+     *     name="getPrincipal"
+     * )
+     */
+    public function getPrincipal($id,PromosRepository $promosRepository)
+    {
+        $this->canViewPromo();
+        $promoTab = [];
+        $promo = $promosRepository->findOneBy(["id" => $id]);
+        if ($promosRepository && !$promo->getIsDeleted())
+        {
+            $groupes = $promo->getGroupes();
+            foreach ($groupes as $groupe )
+            {
+                if($groupe->getType() == "principal")
+                {
+                    $promoTab["apprenants"] = $groupe->getApprenant();
+                    break;
+                }
+            }
+            $promoTab["referentiel"] = $promo->getReferentiel();
+            $promoTab["formateur"] = $promo->getFormateur();
+            if(!isset($promoTab["apprenants"]))
+            {
+                return $this->json(["message" => "Pas de groupe princpal pour cette promo."]);
+            }
+            return $this->json($promoTab,Response::HTTP_OK);
+        }
+        return $this->json(["message" => "Ressource inexistante."],Response::HTTP_NOT_FOUND);
+    }
+
+    /**
+     * @Route(
+     *     path="/api/admin/promos/{id}/referentiels",
+     *     methods={"GET"},
+     *     name="getReferentiel"
+     * )
+     */
+    public function getReferentiel($id,PromosRepository $promosRepository)
+    {
+        $this->canViewPromo();
+        $promo = $promosRepository->findOneBy(["id" => $id]);
+        if ($promo && !$promo->getIsDeleted())
+        {
+            $promoTab = [];
+            $referentiel = $promo->getReferentiel();
+            $grpeCompetences = $referentiel->getGroupeCompetence();
+            $promoTab["referentiel"] = $referentiel;
+            $promoTab["promo"] = $referentiel->getPromos();
+            $promoTab["grpeCompetences"] = $grpeCompetences;
+            $promoTab["competences"] = [];
+            foreach ($grpeCompetences  as $groupeCompetence)
+            {
+                $competences = $groupeCompetence->getCompetences();
+                foreach ($competences as $competence)
+                {
+                    $promoTab["competences"][] = $competence;
+                }
+            }
+            return $this->json($promoTab,Response::HTTP_OK);
+        }
+        return $this->json(["message" => "Ressource inexistante."],Response::HTTP_NOT_FOUND);
+    }
+
+    /**
+     * @Route(
+     *     path="/api/admin/promos/{id}/apprenants/attente",
+     *     methods={"GET"},
+     *     name="getWaitingStudent"
+     * )
+     */
+    public function getWaitingStudent($id,PromosRepository $promosRepository)
+    {
+        $this->canViewPromo();
+        $promo = $promosRepository->findOneBy(["id" => $id]);
+        if ($promo && !$promo->getIsDeleted())
+        {
+            $promoTab = [];
+            $groupes = $promo->getGroupes();
+            $promoTab["referentiel"] = $promo->getReferentiel();
+            $promoTab["apprenants"] = [];
+            foreach ($groupes as $groupe)
+            {
+                $students = $groupe->getApprenant();
+                foreach ($students as $student)
+                {
+                    if(!$student->getIsConnected())
+                    {
+                        $promoTab["apprenants"][] = $student;
+                    }
+                }
+            }
+            return $this->json($promoTab,Response::HTTP_OK);
+        }
+        return $this->json(["message" => "Ressource inexistante."],Response::HTTP_NOT_FOUND);
+    }
+
+    /**
+     * @Route(
+     *     path="/api/admin/promos/{idPromo}/groupes/{idGroupe}/apprenants",
+     *     methods={"GET"},
+     *     name="getStudentsInPromo"
+     * )
+     */
+    public function getStudentsInPromo($idPromo,$idGroupe,PromosRepository $promosRepository)
+    {
+        $this->canViewPromo();
+        $promo = $promosRepository->findOneBy(["id" => $idPromo]);
+        if($promo && !$promo->getIsDeleted())
+        {
+            $promoTab = [];
+            $groupes = $promo->getGroupes();
+            $promoTab["referentiel"] = $promo->getReferentiel();
+            foreach ($groupes as $groupe )
+            {
+                if($groupe->getId() == $idGroupe)
+                {
+                    $promoTab["groupe"] = $groupe;
+                    $promoTab["apprenants"] = $groupe->getApprenant();
+                    $promoTab["promo"] = $groupe->getPromos();
+                }
+            }
+            if(!isset($promoTab["groupe"]))
+            {
+                return $this->json(["message" => "Cette groupe n'exite pas dans ce promo."],Response::HTTP_NOT_FOUND);
+            }
+            return  $this->json($promoTab,Response::HTTP_OK);
+        }
+        return $this->json(["message" => "Ressource inexistante."],Response::HTTP_NOT_FOUND);
+    }
+
+    /**
+     * @Route(
+     *     path="/api/admin/promos/{id}/formateurs",
+     *     methods={"GET"},
+     *     name="getFormateurInProm"
+     * )
+     */
+    public function getFormateurInProm($id,PromosRepository $promosRepository)
+    {
+        $this->canViewPromo();
+        $promo = $promosRepository->findOneBy(["id" => $id]);
+        if($promo && !$promo->getIsDeleted())
+        {
+            $promoTab = [];
+            $promoTab["referentiel"] = $promo->getReferentiel();
+            $formateurs = $promo->getFormateur();
+            $promoTab["formateurs"] = $formateurs;
+            $promoTab["groupes"] = [];
+            foreach ($formateurs as $formateur )
+            {
+                $groupes = $formateur->getGroupes();
+                foreach ($groupes as $groupe )
+                {
+                    $promoTab["groupes"][] = $groupe;
+                }
+            }
+            return  $this->json($promoTab,Response::HTTP_OK);
+        }
+        return $this->json(["message" => "Ressource inexistante."],Response::HTTP_NOT_FOUND);
+    }
+    
     /**
      * @Route(
      *     path="/api/admin/promos",
@@ -202,6 +439,7 @@ class PromosController extends AbstractController
         $this->manager->flush();
         return $this->json($promoObj,Response::HTTP_CREATED);
     }
+
 
     private function canViewPromo()
     {
